9dd56593c984f4623774e34b2c97b8521b41b21f
pratikraundal12 <pratikraudale12@gmail.com>
2024-10-06 03:21:28 +0530

    First Commit To Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace SuperMart_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            imageList1 = new ImageList(components);
+            imageList2 = new ImageList(components);
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            groupBox3 = new GroupBox();
+            SuperMart = new Label();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // imageList2
+            // 
+            imageList2.ColorDepth = ColorDepth.Depth32Bit;
+            imageList2.ImageSize = new Size(16, 16);
+            imageList2.TransparentColor = Color.Transparent;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackgroundImage = (Image)resources.GetObject("groupBox1.BackgroundImage");
+            groupBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            groupBox1.Location = new Point(-2, 32);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(371, 453);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackgroundImage = (Image)resources.GetObject("groupBox2.BackgroundImage");
+            groupBox2.CausesValidation = false;
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Location = new Point(375, 65);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(376, 420);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Login";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Cursor = Cursors.Hand;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(234, 276);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 43);
+            button2.TabIndex = 7;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SeaGreen;
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(48, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 43);
+            button1.TabIndex = 6;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Image = (Image)resources.GetObject("label3.Image");
+            label3.Location = new Point(6, 64);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Role";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Select Role", "Admin", "Saller" });
+            comboBox1.Location = new Point(103, 61);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(262, 33);
+            comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Image = (Image)resources.GetObject("label2.Image");
+            label2.Location = new Point(6, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.Location = new Point(6, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(103, 190);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(262, 31);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 123);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(262, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = Color.DodgerBlue;
+            groupBox3.Controls.Add(SuperMart);
+            groupBox3.Location = new Point(-2, -22);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(748, 81);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            // 
+            // SuperMart
+            // 
+            SuperMart.AutoSize = true;
+            SuperMart.ForeColor = Color.White;
+            SuperMart.Location = new Point(325, 31);
+            SuperMart.Name = "SuperMart";
+            SuperMart.Size = new Size(95, 25);
+            SuperMart.TabIndex = 6;
+            SuperMart.Text = "SuperMart";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(752, 484);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Supermart";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ImageList imageList1;
+        private ImageList imageList2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox textBox1;
+        private GroupBox groupBox3;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label SuperMart;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace SuperMart_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
